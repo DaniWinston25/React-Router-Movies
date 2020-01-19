ea577e4ee9c9d263d2ae6961cd02c17ce5548b72
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2020-01-19 12:51:11 -0500

    updates to MovieCard

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MovieCard = props => {
-  return (props) ;
+  const {movie} = props
+  const [title, getTitle] = useState([]);
+
+  useEffect(() => {
+    let id = props.match.params.id;
+props.movie.find((movie)=>{
+  getTitle (movie.id === id)
+})}
+
+  , [])
+  
+  return ;
 };
 
 export default MovieCard;
