30ed178c79bc64e33b002858cb95b9b2fbe48534
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2020-01-20 21:14:46 -0500

    home button, no indv movie yet

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
@@ -30,9 +31,10 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars,id } = movie;
   return (
     <div className="save-wrapper">
+      <Link to= {`/movie/${id}`}>
       <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
@@ -49,6 +51,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
+      </Link>
       <div className="save-button">Save</div>
     </div>
   );
